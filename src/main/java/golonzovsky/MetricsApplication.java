077ec0308aa29eaa889b9b1d6c18cdc3313a3d69
golonzovsky <alex.golonzovsky@gmail.com>
2017-03-29 11:42:18 +0200

    bind to localhost:8888, test endpoints

--- src/main/java/golonzovsky/MetricsApplication.java
+++ src/main/java/golonzovsky/MetricsApplication.java
@@ -2,11 +2,34 @@ package golonzovsky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MetricsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MetricsApplication.class, args);
+    }
+}
+
+@RestController
+class RestEndpoints {
+
+    @GetMapping("one")
+    String one() throws InterruptedException {
+        Thread.sleep(1000);
+        return "one";
+    }
+
+    @GetMapping("half")
+    String half() throws InterruptedException {
+        Thread.sleep(500);
+        return "half";
+    }
+
+    @GetMapping("instant")
+    String instant() throws InterruptedException {
+        return "instant";
     }
 }

4cf63c93e85432e7d26c9012433426a722d4031d
golonzovsky <alex.golonzovsky@gmail.com>
2017-03-30 16:57:06 +0200

    simpleclient_spring_boot

--- src/main/java/golonzovsky/MetricsApplication.java
+++ src/main/java/golonzovsky/MetricsApplication.java
@@ -1,42 +1,22 @@
 package golonzovsky;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.codahale.metrics.ConsoleReporter;
-import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Slf4jReporter;
-import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
-import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 
 @SpringBootApplication
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class MetricsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MetricsApplication.class, args);
     }
 }
-
-/*@Configuration
-@EnableMetrics
-class SpringConfiguringClass extends MetricsConfigurerAdapter {
-
-    @Override
-    public void configureReporters(MetricRegistry metricRegistry) {
-        // registerReporter allows the MetricsConfigurerAdapter to
-        // shut down the reporter when the Spring context is closed
-        registerReporter(Slf4jReporter
-                .forRegistry(metricRegistry)//.outputTo(Logger)
-                .build())
-                .start(1, TimeUnit.MINUTES);
-    }
-
-}*/
 
 @RestController
 class RestEndpoints {

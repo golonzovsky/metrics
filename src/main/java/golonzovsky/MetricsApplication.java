ec08c3580b88da4388feb5b0b78cffb4d422940f
golonzovsky <alex.golonzovsky@gmail.com>
2017-03-30 15:57:52 +0200

    prometheus endpoint initial

--- src/main/java/golonzovsky/MetricsApplication.java
+++ src/main/java/golonzovsky/MetricsApplication.java
@@ -1,9 +1,18 @@
 package golonzovsky;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Slf4jReporter;
+import com.ryantenney.metrics.spring.config.annotation.EnableMetrics;
+import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
 
 @SpringBootApplication
 public class MetricsApplication {
@@ -12,6 +21,22 @@ public class MetricsApplication {
         SpringApplication.run(MetricsApplication.class, args);
     }
 }
+
+/*@Configuration
+@EnableMetrics
+class SpringConfiguringClass extends MetricsConfigurerAdapter {
+
+    @Override
+    public void configureReporters(MetricRegistry metricRegistry) {
+        // registerReporter allows the MetricsConfigurerAdapter to
+        // shut down the reporter when the Spring context is closed
+        registerReporter(Slf4jReporter
+                .forRegistry(metricRegistry)//.outputTo(Logger)
+                .build())
+                .start(1, TimeUnit.MINUTES);
+    }
+
+}*/
 
 @RestController
 class RestEndpoints {
---
+++ src/main/java/golonzovsky/prometheus/PrometheusEndpoint.java
@@ -0,0 +1,37 @@
+package golonzovsky.prometheus;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.exporter.common.TextFormat;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by jzietsman on 1/28/16.
+ */
+@Slf4j
+class PrometheusEndpoint extends AbstractEndpoint<String> {
+
+    private CollectorRegistry registry;
+
+    PrometheusEndpoint(CollectorRegistry registry) {
+        super("prometheus",false, true);
+        this.registry = registry;
+    }
+
+    @Override
+    public String invoke() {
+        Writer writer = new StringWriter();
+        try {
+            TextFormat.write004(writer, registry.metricFamilySamples());
+        } catch (IOException e) {
+            log.error("error writing prometheus metrics to endpoint");
+        }
+        return writer.toString();
+    }
+}

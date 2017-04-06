524531cb4d8573726ab4e6655e8b2c4f60c23267
golonzovsky <alex.golonzovsky@gmail.com>
2017-04-06 09:34:02 +0200

    gauges to timers

--- src/main/java/golonzovsky/MetricsApplication.java
+++ src/main/java/golonzovsky/MetricsApplication.java
@@ -1,9 +1,15 @@
 package golonzovsky;
 
+import java.security.SecureRandom;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.metrics.dropwizard.DropwizardMetricServices;
+import org.springframework.boot.actuate.metrics.dropwizard.ReservoirFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,35 +28,54 @@ public class MetricsApplication {
     }
 }
 
-/*@Configuration
+@Configuration
 @EnableMetrics
 class SpringConfiguringClass extends MetricsConfigurerAdapter {
+
+    @Bean
+    DropwizardMetricServices dropwizardMetricServices(MetricRegistry metricRegistry,
+                                                      ObjectProvider<ReservoirFactory> resFactoryProvider)  {
+        return new DropwizardMetricServices(metricRegistry, resFactoryProvider.getIfAvailable()){
+            @Override
+            public void submit(String name, double value) {
+                if (name.startsWith("response.")){
+                    super.submit("timer." + name, value);
+                } else {
+                    super.submit(name, value);
+                }
+            }
+        };
+    }
 
     @Override
     public void configureReporters(MetricRegistry metricRegistry) {
         // registerReporter allows the MetricsConfigurerAdapter to
         // shut down the reporter when the Spring context is closed
-        registerReporter(Slf4jReporter
+        registerReporter(ConsoleReporter
                 .forRegistry(metricRegistry)//.outputTo(Logger)
                 .build())
                 .start(1, TimeUnit.MINUTES);
     }
 
-}*/
+}
 
 @RestController
 class RestEndpoints {
 
+    private Random random = new SecureRandom();
+
     @GetMapping("one")
     String one() throws InterruptedException {
-        Thread.sleep(1000);
-        return "one";
+        int millis = random.nextInt(1000);
+        Thread.sleep(millis);
+        return String.valueOf(millis);
     }
 
     @GetMapping("half")
     String half() throws InterruptedException {
-        Thread.sleep(500);
-        return "half";
+        int millis = random.nextInt(500);
+        Thread.sleep(millis);
+        return String.valueOf(millis);
     }
 
     @GetMapping("instant")
